0d5376a8f585d44cd34e14de9e4bee759c8f8d41
Raunak Ray <raunakay3260@gmail.com>
2026-06-09 05:05:14 +0530

    feat: add initial frontend setup with React, TypeScript, and Tailwind CSS

---
+++ spotify-clone/frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+  return (
+    <div className=" text-orange-400">
+      App
+      <Button variant={"link"}>Button</Button>
+    </div>
+  )
+}
+
+export default App
