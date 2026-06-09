5d3480ff512bffff1a569bcb14fbad5d53dd3952
Raunak Ray <raunakay3260@gmail.com>
2026-06-09 09:48:50 +0530

    feat: implement user and admin routes, and establish MongoDB connection

--- spotify-clone/backend/src/server.js
+++ spotify-clone/backend/src/server.js
@@ -1,14 +1,21 @@
 import express from "express";
 import "dotenv/config";
+import userRoutes from "./routes/user.route.js";
+import adminRoutes from "./routes/admin.route.js";
+import { connectToDb } from "../lib/db.js";
+import { setServers } from "node:dns/promises";
+
+setServers(["1.1.1.1", "8.8.8.8"]);
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(express.json());
+app.use("/api/users", userRoutes);
+app.use("/api/admin", adminRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server started on http://localhost:${PORT}`);
+  connectToDb();
 });
